cc79e0bac787b0f4ba17f3a307cb2b0f293f1b88
ArchLeaders <archparliament@gmail.com>
2022-02-17 01:42:26 -0800

    Actornode transform properties

--- ui/nodes.py
+++ ui/nodes.py
@@ -12,7 +12,39 @@ class Actor(Node, REF_MubinLinkEditor_NodeTree):
     bl_width_min = 160.0
 
     definition: bpy.props.StringProperty(name='', default='LinkTagAnd')
-    hash_id: bpy.props.StringProperty(name='', description='The dummy actor\'s HashID as a decimal.\nThis value will not be used in the exported JSON')
+    hash_id: bpy.props.StringProperty(
+        name='',
+        description='The dummy actor\'s HashID as a decimal.\nThis value will not be used in the exported JSON')
+
+    scale: bpy.props.FloatVectorProperty(
+        name='',
+        default=(1, 1, 1),
+        max=1000,
+        min=0,
+        description='XYZ (Y is UP) | Leave as 0, 0, 0 for automatic placement'
+    )
+    rotation: bpy.props.FloatVectorProperty(
+        name='',
+        default=(0, 0, 0),
+        max=360,
+        min=0,
+        description='XYZ (Y is UP) | Leave as 0, 0, 0 for automatic placement'
+    )
+    translate: bpy.props.FloatVectorProperty(
+        name='',
+        default=(0, 0, 0),
+        max=5000,
+        min=-5000,
+        description='XYZ (Y is UP) | Leave as 0, 0, 0 for automatic placement'
+    )
+
+    ref_object: bpy.props.PointerProperty(
+        type=bpy.types.Object,
+        name='Reference Object',
+        description='Link an object to use it\'s transform values.\n' +
+            'Note: It you apply the objects transform, all values will be set to (0, 0, 0)'
+    )
+
     params: bpy.props.CollectionProperty(type=ParamProperties)
     params_index: bpy.props.IntProperty(name='Params Index', default=0)
     
@@ -37,33 +69,64 @@ class Actor(Node, REF_MubinLinkEditor_NodeTree):
 
     # Properties only in the properties panel
     def draw_buttons_ext(self, context, layout):
+        # Actor Name
         layout.label(text='Actor Name')
-        layout.prop(self, 'definition')
-        layout.label(text='Parameters')
+        layout.prop(self, 'definition', icon='OUTLINER_DATA_GP_LAYER')
+        layout.separator()
 
+
+        # Params
+        layout.label(text='Parameters')
         row = layout.row()
         row.template_list("MLE_UL_Params", "Params", self, "params", self, "params_index")
-        col = row.column()
-        col.ui_units_x = 1.0
 
+        col = row.column()
+
+        col.ui_units_x = 1.0
         sub_col = col.column(align=True)
         sub_col.operator('params.add', text='', icon='ADD')
         sub_col.operator('params.remove', text='', icon='REMOVE')
-
         sub_col.separator(factor=1.8)
+
         sub_col.operator('params.move', text='', icon='TRIA_UP').direction = 'UP'
         sub_col.operator('params.move', text='', icon='TRIA_DOWN').direction = 'DOWN'
-
         sub_col.separator(factor=1.8)
+
         sub_col.operator('params.clear', text='', icon='PANEL_CLOSE')
 
+
+        # Param controls
         row = layout.row(align=True)
         row.operator('params.load')
         row.operator('params.copy')
         row.operator('params.import')
+        layout.separator()
 
+
+        # Transform
+        layout.label(text='Transforms')
+
+        row = layout.row()
+
+        col = row.column()
+        col.label(text='Scale', icon='OBJECT_ORIGIN')
+        col.prop(self, 'scale')
+
+        col = row.column()
+        col.label(text='Rotation', icon='EMPTY_AXIS')
+        col.prop(self, 'rotation')
+
+        col = row.column()
+        col.label(text='Translate', icon='EMPTY_ARROWS')
+        col.prop(self, 'translate')
+
+        layout.prop(self, 'ref_object', icon='OBJECT_DATA', text='')
+
+        layout.separator()
+
+        # Hash ID
         layout.label(text='Hash Identifier')
-        layout.prop(self, 'hash_id')
+        layout.prop(self, 'hash_id', icon='COPY_ID')
 
 class Link(Node):
     """Simple node to link two actors with a signal"""
