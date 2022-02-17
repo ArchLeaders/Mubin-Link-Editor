0506015219d9afa9f92820eb51b0b2719bc97cb0
ArchLeaders <archparliament@gmail.com>
2022-02-16 20:48:18 -0800

    Register bpy elements

---
+++ registry.py
@@ -0,0 +1,88 @@
+from .ui.nodes import (
+    Actor,
+    Link,
+    Template,
+)
+from .ui.node_tree import (
+    MubinLinkEditor_NodeTree,
+)
+from .operators import (
+    MLE_EXPORTER_OT_ExportTemplate,
+    MLE_EXPORTER_OT_SaveTemplate,
+
+    MLE_PARAMS_OT_Add,
+    MLE_PARAMS_OT_Remove,
+    MLE_PARAMS_OT_MoveItem,
+    MLE_PARAMS_OT_Clear,
+    MLE_PARAMS_OT_Load,
+    MLE_PARAMS_OT_Import,
+    MLE_PARAMS_OT_Copy,
+    MLE_NODES_OT_Save,
+    MLE_NODES_OT_Sync,
+    MLE_NODES_OT_Add,
+    MLE_NODES_OT_Remove,
+    MLE_NODES_OT_MoveItem,
+    MLE_NODES_OT_Clear,
+    MLE_IGNORE_OT_Save,
+    MLE_IGNORE_OT_Sync,
+    MLE_IGNORE_OT_Add,
+    MLE_IGNORE_OT_Remove,
+    MLE_IGNORE_OT_MoveItem,
+    MLE_IGNORE_OT_Clear,
+)
+from .ui.panels import (
+    MLE_SETTINGS_PT_Nodes,
+    MLE_SETTINGS_PT_Ignored,
+)
+from .ui.props import (
+    ParamProperties,
+    MLE_UL_Params
+)
+
+classes = (
+    # Node trees
+    MubinLinkEditor_NodeTree,
+
+    # Operators
+        # OT_EXPORTER
+        MLE_EXPORTER_OT_ExportTemplate,
+        MLE_EXPORTER_OT_SaveTemplate,
+
+        # OT_PARAMS
+        MLE_PARAMS_OT_Add,
+        MLE_PARAMS_OT_Remove,
+        MLE_PARAMS_OT_Clear,
+        MLE_PARAMS_OT_Load,
+        MLE_PARAMS_OT_Import,
+        MLE_PARAMS_OT_Copy,
+        MLE_PARAMS_OT_MoveItem,
+
+        # OT_NODES
+        MLE_NODES_OT_Save,
+        MLE_NODES_OT_Sync,
+        MLE_NODES_OT_Add,
+        MLE_NODES_OT_Remove,
+        MLE_NODES_OT_MoveItem,
+        MLE_NODES_OT_Clear,
+
+        # OT_IGNORE
+        MLE_IGNORE_OT_Save,
+        MLE_IGNORE_OT_Sync,
+        MLE_IGNORE_OT_Add,
+        MLE_IGNORE_OT_Remove,
+        MLE_IGNORE_OT_MoveItem,
+        MLE_IGNORE_OT_Clear,
+
+    # Panels
+    MLE_SETTINGS_PT_Nodes,
+    MLE_SETTINGS_PT_Ignored,
+
+    # Props
+    ParamProperties,
+    MLE_UL_Params,
+
+    # Nodes
+    Actor,
+    Link,
+    Template,
+)
