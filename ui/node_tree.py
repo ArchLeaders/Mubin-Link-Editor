dc3617c47906d82539efc4ac427b762d1143716a
ArchLeaders <archparliament@gmail.com>
2022-02-16 20:51:27 -0800

    Define node tree

---
+++ ui/node_tree.py
@@ -0,0 +1,11 @@
+from bpy.types import NodeTree
+
+class MubinLinkEditor_NodeTree(NodeTree):
+    """Node editor for editing and viewing BotW Map Unit linked actors."""
+    bl_label = 'Mubin Link Editor'
+    bl_icon = 'MOD_PARTICLE_INSTANCE'
+
+class REF_MubinLinkEditor_NodeTree:
+    @classmethod
+    def poll(cls, ntree):
+        return ntree.bl_idname == 'MubinLinkEditor_NodeTree'
